61440516266c1c67ebb273edf4c270e3a339b69c
Awais Qureshi <awais.qureshi@arbisoft.com>
2025-10-17 15:40:35 +0500

    Modify AWS storage settings in tests/settings.py
    Updated AWS_STORAGE_BUCKET_NAME and added STORAGES configuration.

--- tests/settings.py
+++ tests/settings.py
@@ -6,4 +6,18 @@ SECRET_KEY = "tests"
 
 USE_TZ = True
 
-AWS_STORAGE_BUCKET_NAME = "test-bucket"
+
+AWS_STORAGE_BUCKET_NAME = "default-bucket"
+
+INSTALLED_APPS = ["django.contrib.auth", "django.contrib.contenttypes", "storages", "smart_storages"]
+
+STORAGES = {
+    "import_export": {
+        "BACKEND": "tests.test_storages.ImportExportS3Storage",
+        "OPTIONS": {"custom_domain": None, "querystring_auth": True, "bucket_name": "import_export"},
+    },
+    "analytics": {
+        "BACKEND": "tests.test_storages.AnalyticsS3Storage",
+        "OPTIONS": {"custom_domain": None, "bucket_name": "analytics"},
+    },
+}
